b87b76339fdced745974f86c831a23387fce4de5
ajiths10 <ajithsnair10@gmail.com>
2022-06-24 20:27:07 +0530

    Auto writing text

--- dev.js
+++ dev.js
@@ -25,3 +25,18 @@ const createNotification= () => {
         notify.remove();
     }, 3000);
 }
+
+//Auto writing text
+
+const text = 'Hello world !!';
+let index = 0;
+
+function writeText() {
+    document.body.innerText = text.slice(0, index);
+    index++;
+    if(index > text.length - 1){
+        index = 0;
+    }
+}
+
+setInterval(writeText,100)
